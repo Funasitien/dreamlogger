e88a1fbac7d246afd83958067b7e0c14cde4cb8a
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2024-01-11 09:38:19 +0100

    Update dreamlogger.py

--- dreamlogger.py
+++ dreamlogger.py
@@ -1,23 +1,29 @@
 #--------------------------------------------------#
 # DreamLogger                        - By Dreamcloud
 #--------------------------------------------------#
-# Suport @ discord.dreamcloud.tk
+# Suport @ dreamclouds.fr/discord
 # Full creadits to @Funasitien
 #
 # github.com/funasitien
-# 
+#   
+
+import os
 
 class color:
     b = '\033[94m' # Blue
     c = '\033[96m' # Cyan
     g = '\033[92m' # green
     y = '\033[93m' # yellow
+    j = '\033[93m' # jaune
     r = '\033[91m' # red
     k = '\033[0m' #rest (or gray)
 
-
+infotext = "[" + color.b + "INFO" + color.k + "]  "
 warn = "warn"
 error = "error"
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 def debug_mode(value=False):
     global debu
@@ -27,24 +33,24 @@ def debug_mode(value=False):
         debu = False
 
 def info(message):
-    print("[" + color.b + "INFO" + color.k + "] " + message)
+    print("[" + color.b + "INFO" + color.k + "]  ", message)
 
 def warn(message):
-    print("[" + color.y + "WARN" + color.k + "] " + message)
+    print("[" + color.y + "WARN" + color.k + "]  ", message)
 
 
 def error(message):
-    print("[" + color.r + "ERROR" + color.k + "] " + message)
+    print("[" + color.r + "ERROR" + color.k + "] ", message)
 
 
 def debug(message, type="fine"):
     if debu == False:
         return
     if type == "error":
-        print("[" + color.r + "DEBUG" + color.k + "] " + message)
+        print("[" + color.r + "DEBUG" + color.k, "] ", message)
         return
     if type == "warn":
-        print("[" + color.y + "DEBUG" + color.k + "] " + message)
+        print("[" + color.y + "DEBUG" + color.k, "] ", message)
         return
     else:
-        print("[" + color.g + "DEBUG" + color.k + "] " + message)
+        print("[" + color.g + "DEBUG" + color.k + "] ", message)
